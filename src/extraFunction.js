2b7330ca03b4968bbc37084b6871e25f390b4f1c
Steven Chao <gash612@gmail.com>
2020-08-18 00:41:20 +0800

    [Extra] Add formTableView function

---
+++ src/extraFunction.js
@@ -0,0 +1,39 @@
+function formTableView()
+{
+	var contentByLines = d3.select("#inputTextArea").property("value").split("\n");
+	var rowCount = contentByLines.length;
+	var colCount = contentByLines[0].split("	").length;
+	var contentByColByLines;
+	console.log(rowCount,colCount);
+	var tableContent = "";
+	var tableStructureStart = "<table><tbody>";
+	var tableStructureEnd = "</tbody></table>";
+	tableContent += tableStructureStart; 
+	tableContent += "<tr>";
+	tableContent += "<td>";
+	tableContent += "</td>";
+	for(var col in contentByLines[0].split("	"))
+	{		
+		tableContent += "<td>";
+		tableContent += "col "+(col+1);
+		tableContent += "</td>";		
+	}
+	for(var lineIndex in contentByLines)
+	{
+		tableContent += "<tr>";
+		tableContent += "<td>";
+		tableContent += "row "+(lineIndex+1);
+		tableContent += "</td>";
+		contentByColByLines = contentByLines[lineIndex].split("	");
+		for(var col in contentByColByLines)
+		{
+			tableContent += "<td>";
+			tableContent += contentByColByLines[col];
+			tableContent += "</td>";
+		}
+		tableContent += "</tr>";	
+	}
+	tableContent += tableStructureEnd;
+	d3.select("#modalContent").html(tableContent);
+	showModal();
+}
