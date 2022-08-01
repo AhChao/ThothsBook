b5f3f62bb513ec75bc5fe09a01e18c802d425e10
stevne.chao <gash612@gmail.com>
2022-08-01 08:47:12 +0800

    [Feat] Change source data of time

--- src/timeZone.js
+++ src/timeZone.js
@@ -5,17 +5,36 @@ const app = Vue.createApp({
             useingLocalTime: true,
             localTime: "",
             inputTime: "",
+            timezones: [],//['Abbreviation','Time zone name','Location','Offset']
+            leftSideChosen: "",
+            rightSideChosen: "",
+            rightSideTime: "",
         };
     },
     mounted() {
         this.init();
     },
     computed: {
+        leftSideFilteredTimezones() {
+            return this.timezones.filter(x => x[0].toLowerCase().indexOf(this.leftSideChosen.toLowerCase()) != -1 || this.leftSideChosen == "").map(x => x[0] + " | " + x[1]);
+        },
+        rightSideFilteredTimezones() {
+            return this.timezones.filter(x => x[0].toLowerCase().indexOf(this.rightSideChosen.toLowerCase()) != -1 || this.rightSideChosen == "").map(x => x[0] + " | " + x[1]);
+        }
     },
     methods: {
         init() {
             this.localTime = new Date();
             this.inputTime = new Date();
+            fetch("../data/timezone.json").then(response => response.json()).then(json => { this.timezones = json; });
+        },
+        selectTimeZone(timezoneStr, dir) {
+            if (dir == "left") {
+                this.leftSideChosen = timezoneStr;
+            }
+            else {
+                this.rightSideChosen = timezoneStr;
+            }
         },
     }
 });
